6f569da7b1015674952faccb0a73918b1f8fda58
Jhalak Roy <jhalakroy1988@gmail.com>
2018-05-17 08:08:40 -0700

    Added two more lines on Example.java to fix the bug

--- Example.java
+++ Example.java
@@ -4,7 +4,8 @@ public static void main(String str[]){
 
 
 	System.out.println("I am learning git");
-
+ System.out.println("We are now practicing branch");
+ System.out.println("we are looking merge also.");
 
 }
 
